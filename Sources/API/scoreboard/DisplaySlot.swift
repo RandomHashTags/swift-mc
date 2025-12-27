46a9bed87f3eeaf748a8f3d01972c7374767766c
RandomHashTags <imrandomhashtags@gmail.com>
2025-12-27 16:49:09 -0600

    add two structs relating to scoreboards

---
+++ Sources/API/scoreboard/DisplaySlot.swift
@@ -0,0 +1,23 @@
+
+/// Locations for displaying objectives to the player.
+public enum DisplaySlot: String, CaseIterable, Sendable {
+    case belowName
+    case playerList
+    case sidebar
+    case sidebarAqua
+    case sidebarBlack
+    case sidebarBlue
+    case sidebarDarkAqua
+    case sidebarDarkBlue
+    case sidebarDarkGray
+    case sidebarDarkGreen
+    case sidebarDarkPurple
+    case sidebarDarkRed
+    case sidebarGold
+    case sidebarGray
+    case sidebarGreen
+    case sidebarLightPurple
+    case sidebarRed
+    case sidebarWhite
+    case sidebarYellow
+}
---
+++ Sources/API/scoreboard/RenderType.swift
@@ -0,0 +1,9 @@
+
+/// Controls the way an `Objective` is rendered client side.
+public enum RenderType: String, Sendable {
+    /// Display number of hearts corresponding to value.
+    case hearts
+
+    /// Display integer value.
+    case integer
+}
